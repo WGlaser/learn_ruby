6f1a9711acdc9fdf4b9347320deb1985fc07c229
Wade Glaser <Wade.Glaser@gmail.com>
2018-01-04 13:32:15 -0500

    book_titles updated

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+	attr_reader :title
+
+	def title=(given_title)
+		words = given_title.split(" ")
+    	words = [words[0].capitalize] + words[1..-1].map do |word| #cap first word, then search through the rest of words
+       		 little_words = %w{a an and the in of}
+        	 if little_words.include? word
+         		 word
+        	else
+                word.capitalize
+        	end
+      	end
+    @title = words.join(" ")
+  end
+
 end
